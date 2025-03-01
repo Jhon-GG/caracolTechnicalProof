f48242edbaa360a59f4b06a60055d2f843d2198a
Jhon-GG <111620215+Jhon-GG@users.noreply.github.com>
2025-02-28 23:13:13 -0500

    feat: :construction: Adding the audio
    Implementing the audio playback to the audioplayer

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,21 @@ document.addEventListener('DOMContentLoaded', function() {
         navMenu.classList.toggle('active');
     });
 });
+
+
+
+
+
+const playButton = document.getElementById("playButton");
+    const playIcon = document.getElementById("playIcon");
+    const audioPlayer = document.getElementById("audioPlayer");
+
+    playButton.addEventListener("click", () => {
+        if (audioPlayer.paused) {
+            audioPlayer.play();
+            playIcon.src = "src/assets/images/audioPause.svg"; // Cambia a icono de pausa
+        } else {
+            audioPlayer.pause();
+            playIcon.src = "src/assets/images/audioPlayer.svg"; // Cambia a icono de play
+        }
+    });
